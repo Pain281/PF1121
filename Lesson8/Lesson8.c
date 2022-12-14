d36f81ce30f6631319950a8a7bd9899118295555
Pain281 <an.nguyenthien281@gmail.com>
2022-12-14 14:45:45 +0700

    update code

--- Lesson8/Lesson8.c
+++ Lesson8/Lesson8.c
@@ -251,7 +251,7 @@ void Exercise7()
         else if (str[i] == sc)
         {
             prev = i;
-            if (str[i + 1] >= 'a' && str[i + 1] <= 'z' && str[i - 1] >= 'a' && str[i - 1] <= 'z' || str[i + 1] >= '0' && str[i + 1] <= '9' && str[i - 1] >= 'a' && str[i - 1] <= 'z')
+            if (str[i + 1] >= 'a' && str[i + 1] <= 'z' && str[i - 1] >= 'a' && str[i - 1] <= 'z' || str[i - 1] >= '0' && str[i - 1] <= '9' && str[i + 1] >= 'a' && str[i + 1] <= 'z')
             {
                 scFlag++;
             }
